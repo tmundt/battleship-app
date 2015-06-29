e817cb674874bd816ce3cf89e7e27cf9cd5f1d7b
Tom@Pegasus <info@thomas-mundt.de>
2015-06-29 14:24:05 +0200

    div. Grafiken, MainActivity

--- battleship-android/src/org/nse/battleship/MainActivity.java
+++ battleship-android/src/org/nse/battleship/MainActivity.java
@@ -2,14 +2,30 @@ package org.nse.battleship;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private ImageView startImage;
+    private ImageButton startButton;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        startImage = (ImageView) findViewById(R.id.startImage);
+        startButton = (ImageButton) findViewById(R.id.weiterButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
---
+++ battleship-android/src/org/nse/battleship/SetupPlayerFieldActivity.java
@@ -0,0 +1,16 @@
+package org.nse.battleship;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by thomasmundt on 28.06.15.
+ * Field where player should set up his ships
+ */
+public class SetupPlayerFieldActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+    }
+}
