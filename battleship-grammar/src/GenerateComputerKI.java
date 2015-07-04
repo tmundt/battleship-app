0c1c4025afd8ff3fe19a5e938629a8ebbf4f1d1d
Tom@Pegasus <info@thomas-mundt.de>
2015-07-04 21:15:18 +0200

    Anfang Generator

--- battleship-grammar/src/GenerateComputerKI.java
+++ battleship-grammar/src/GenerateComputerKI.java
@@ -2,8 +2,7 @@
  * Created by thomasmundt on 11.06.15.
  */
 
-import gen.*
-public class GenerateComputerKI extends  {
+public class GenerateComputerKI extends  PlayerAIMovesBaseListener{
     public static void main(String[] args) {
         System.out.println("GENERATOR");
     }
