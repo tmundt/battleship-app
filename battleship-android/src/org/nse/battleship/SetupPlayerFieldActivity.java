52b2f93702641a247af4b1978c93cdf7d473a575
Tom@starblow <tmundt1@gmail.com>
2015-07-20 23:49:23 +0200

    Positionierungen für Schiffe verbessert

--- battleship-android/src/org/nse/battleship/SetupPlayerFieldActivity.java
+++ battleship-android/src/org/nse/battleship/SetupPlayerFieldActivity.java
@@ -27,11 +27,11 @@ public class SetupPlayerFieldActivity extends Activity {
     public ImageView imagePlayfield;
     public ImageButton buttonDrehen;
     public ImageButton buttonSpielen;
-    public ImageView imageShipBiggest;
-    public ImageView imageShipBig;
-    public ImageView imageShipMedium;
-    public ImageView imageShipMedium2;
-    public ImageView imageShipSmall;
+    public ImageView imageShipCarrier;
+    public ImageView imageShipCruiser;
+    public ImageView imageShipGunboat;
+    public ImageView imageShipSubmarine;
+    public ImageView imageShipSpeedboat;
 
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
@@ -42,33 +42,29 @@ public class SetupPlayerFieldActivity extends Activity {
         imagePlayfield = (ImageView) findViewById(R.id.imagePlayfield);
         buttonDrehen = (ImageButton) findViewById(R.id.buttonDrehen);
         buttonSpielen = (ImageButton) findViewById(R.id.buttonSpielen);
-        imageShipBiggest = (ImageView) findViewById(R.id.imageShipBiggest);
-        imageShipBig = (ImageView) findViewById(R.id.imageShipBig);
-        imageShipMedium = (ImageView) findViewById(R.id.imageShipMedium);
-        imageShipMedium2 = (ImageView) findViewById(R.id.imageShipMedium2);
-        imageShipSmall = (ImageView) findViewById(R.id.imageShipSmall);
+        imageShipCarrier = (ImageView) findViewById(R.id.imageShipCarrier);
+        imageShipCruiser = (ImageView) findViewById(R.id.imageShipCruiser);
+        imageShipGunboat = (ImageView) findViewById(R.id.imageShipGunboat);
+        imageShipSubmarine = (ImageView) findViewById(R.id.imageShipSubmarine);
+        imageShipSpeedboat = (ImageView) findViewById(R.id.imageShipSpeedboat);
 
         // Set tags to identify imageviews = different ships
-        imageShipBiggest.setTag("shipBiggest");
-        imageShipBig.setTag("shipBig");
-        imageShipMedium.setTag("shipMedium");
-        imageShipMedium2.setTag("shipMedium2");
-        imageShipSmall.setTag("shipSmall");
+        imageShipCarrier.setTag("shipCarrier");
+        imageShipCruiser.setTag("shipCruiser");
+        imageShipGunboat.setTag("shipGunboat");
+        imageShipSubmarine.setTag("shipSubmarine");
+        imageShipSpeedboat.setTag("shipSpeedboat");
         imagePlayfield.setTag("imagePlayfield");
 
-        imageShipBiggest.setOnTouchListener(new MyTouchListener());
-        imageShipBig.setOnTouchListener(new MyTouchListener());
-        imageShipMedium.setOnTouchListener(new MyTouchListener());
-        imageShipMedium2.setOnTouchListener(new MyTouchListener());
-        imageShipSmall.setOnTouchListener(new MyTouchListener());
+        imageShipCarrier.setOnTouchListener(new MyTouchListener());
+        imageShipCruiser.setOnTouchListener(new MyTouchListener());
+        imageShipGunboat.setOnTouchListener(new MyTouchListener());
+        imageShipSubmarine.setOnTouchListener(new MyTouchListener());
+        imageShipSpeedboat.setOnTouchListener(new MyTouchListener());
 
-//        findViewById(R.id.imageShipBiggest).setOnDragListener(new MyDragListener());
-//        findViewById(R.id.imageShipMedium).setOnDragListener(new MyDragListener());
-//        findViewById(R.id.imageShipMedium2).setOnDragListener(new MyDragListener());
-//        findViewById(R.id.imageShipBig).setOnDragListener(new MyDragListener());
-//        findViewById(R.id.imageShipSmall).setOnDragListener(new MyDragListener());
-        findViewById(R.id.button_a1).setOnDragListener(new MyDragListener());
         imagePlayfield.setOnDragListener(new MyDragListener());
+        RelativeLayout layout = (RelativeLayout)imagePlayfield.getParent();
+        layout.setOnDragListener(new MyDragListener());
 
     }
 
@@ -87,8 +83,12 @@ public class SetupPlayerFieldActivity extends Activity {
                     return true;
                 case MotionEvent.ACTION_UP:
                     Log.d("ACTION_UP", "released ship");
+                    break;
                 case MotionEvent.ACTION_CANCEL:
                     Log.e("ACTION_CANCEL", "ship x-coord:" + view.getX());
+                    view.setVisibility(View.VISIBLE);
+                    break;
+
             }
             return true;
         }
@@ -100,18 +100,24 @@ public class SetupPlayerFieldActivity extends Activity {
         @Override
         public boolean onDrag(View v, DragEvent event) {
             int action = event.getAction();
+            String viewTag = (String) v.getTag();
+            float x = event.getX();
+            float y = event.getY();
+
             switch (event.getAction()) {
                 case DragEvent.ACTION_DRAG_STARTED:
-                    // do nothing
+                    Log.i("ACTION_DRAG_STARTED","target view is: " +v.getTag());
+                    Log.i("ACTION_DRAG_STARTED","x ist: " +x+", y: "+ y);
                     break;
                 case DragEvent.ACTION_DRAG_ENTERED:
                     Log.d("ACTION_DRAG_ENTERED","entered:" + v.getTag());
+                    Log.i("ACTION_DRAG_STARTED", "x ist: " + x + ", y: " + y);
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
 
                     break;
                 case DragEvent.ACTION_DROP:
-                    String viewTag = (String) v.getTag();
+                    //String viewTag = (String) v.getTag();
                     Log.i("ACTION_DROP","target view is: " +v.getTag());
                     Log.i("ACTION_DROP","view v is: " + v);
                     if (v != imagePlayfield) {
@@ -122,12 +128,89 @@ public class SetupPlayerFieldActivity extends Activity {
                     }
                     // Dropped, reassign View to ViewGroup
                     View dragView = (View) event.getLocalState();
-                    float X = event.getX();
-                    float Y = event.getY();
+                    String dragTag = dragView.getTag().toString();
+//                    float X = event.getX();
+//                    float Y = event.getY();
+                    // Korrektur Positionen/Berechnung Modifizierung der Y-Koordinate
+                    int modifierY;
+                    switch (dragTag) {
+                        case "shipCarrier":
+                            modifierY = 4;
+                            break;
+                        case "shipCruiser":
+                            modifierY = 5;
+                            break;
+                        case "shipGunboat":
+                            modifierY = 7;
+                            break;
+                        case "shipSubmarine":
+                            modifierY = 18;
+                            break;
+                        case "shipSpeedboat":
+                            Log.i("ACTION_DROP","detected Speedboat");
+                            modifierY = 44;
+                            break;
+                        default:
+                            modifierY = 4;
+                    }
+                    Log.d("ACTION_DROP:","VOR Korrektur, x ist: " + (int) x + ",y ist: " + (int) y);
+                    ;
+                    x = x - (dragView.getWidth()/2);
+                    y = y - (dragView.getHeight()/modifierY);
+                    Log.d("ACTION_DROP:","NACH Korrektur, x ist: " + (int) x + ",y ist: " + (int) y);
+                    // Begrenze den linken und rechten Rand
+                    if(x < 140) {
+                        Log.e("ACTION_DROP","zu weit links: " + x);
+                        x = 140;
+                    }
 
-                    Log.d("ACTION_DROP:","X " + (int) X + "Y " + (int) Y);
-                    dragView.setX(X);
-                    dragView.setY(Y);
+                    if(x > 965) {
+                        x = 965;
+                    }
+
+                    // Begrenze nach oben
+                    if(y < 249) {
+                        Log.e("ACTION_DROP","zu weit oben: " + y);
+                        y = 249;
+                    }
+
+                    //Begrenze nach unten, schiffsabhängig
+                    switch (dragTag) {
+                        case "shipCarrier":
+                            if (y > 583) {
+                                Log.e("ACTION_DROP","zu weit unten: " + y);
+                                y = 583;
+                            }
+                            break;
+                        case "shipCruiser":
+                            if (y > 690) {
+                                Log.e("ACTION_DROP","zu weit unten: " + y);
+                                y = 690;
+                            }
+                            break;
+                        case "shipGunboat":
+                        case "shipSubmarine":
+                            if (y > 805) {
+                                Log.e("ACTION_DROP","zu weit unten: " + y);
+                                y = 805;
+                            }
+                            break;
+                        case "shipSpeedboat":
+                            if (y > 924) {
+                                Log.e("ACTION_DROP","zu weit unten: " + y);
+                                y = 924;
+                            }
+                    }
+
+                    // Speichere Schiff in Map ab
+                    // rechne Koordinaten in Zellen
+                    // Pruefe ob bereits belegt
+                    // Falls belegt: kein Drop und kein Speichern
+
+
+                    // Vermeide das Übereinanderlegen von Schiffen
+                    dragView.setX(x);
+                    dragView.setY(y);
                     dragView.setVisibility(View.VISIBLE);
 //                    ViewGroup owner = (ViewGroup) dragView.getParent();
 //                    owner.removeView(dragView);
@@ -135,19 +218,20 @@ public class SetupPlayerFieldActivity extends Activity {
 //                    container.addView(dragView);
                     //dragView.setVisibility(View.VISIBLE);
                     Log.d("ACTION_DROP","dropped:" + dragView.getTag());
+                    Log.d("ACTION_DROP:","x ist: " + (int) x + "y ist: " + (int) y);
                     break;
                 case DragEvent.ACTION_DRAG_ENDED:
-                    Log.d("ACTION_DRAG_ENDED","dropped:" + v.getId());
+                    Log.d("ACTION_DRAG_ENDED","dropped:" + v.getParent().toString());
+                    if (!event.getResult()) {
+                        // Nicht gedroppt, d.h. kein "result"
+                        v.setVisibility(View.VISIBLE);
+                        Log.e("ACTION_DRAG_ENDED", "not in drop zone:" + v.getTag());
+                        return false;
+                    }
                 default:
                     return false;
             }
             return true;
         }
-
-
     }
-
-
 }
-
-
